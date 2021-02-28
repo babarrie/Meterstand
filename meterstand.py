fbd2fb2cd937a70e03294a9db7458edc5c0c8556
babarrie <warnier.mathias@gmail.com>
2021-02-28 14:41:19 +0100

    png generation

--- meterstand.py
+++ meterstand.py
@@ -15,30 +15,33 @@ def show_readings():
     data = pd.read_csv(FILE_NAME,";")
     return render_template('index.html', tables=[data.to_html(header=True, index=False, table_id="table")], titles=[data.columns.values])
 
-@app.route("/charts")
-def show_graph():
-    plt.plot()
-    plt.title()
-    plt.savefig('static/img/graph.png')
-    return render_template('charts.html',url='/static/img/graph.png' )
+@app.route("/charts/<gwe>")
+def show_graph(gwe):
+    fig, axs = plt,plt.subplots()
+    data = pd.read_csv(FILE_NAME,";", index_col=0, parse_dates=True)
+    data.plot.area()
+    axs.set_ylabel(gwe)
+    plt.title(gwe)
+    fig.savefig('~/Projects/Meterstand/static/img/graph.png')
+    return render_template('charts.html',url='~/Projects/Meterstand/static/img/graph.png' ) #vind bestand niet
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-def ingave():
-    df = pd.DataFrame(
-        {
-            "Datum": [],
-            "Gas": [],
-            "Water": [],
-            "Electriciteit": []
-        }
-    )
-    n = 0
-    datum = date.today()
-    while n != 5:
-        datastring = input("Geef meterstand in datum,gas,water,elec:")
-        df = df.append({"Datum": [datum], "Gas": [gas], "Water": [water], "Electriciteit": [ele]},ignore_index=True)
-        print(n)
-    df.to_csv(FILE_NAME,mode='a', index=False)
+#def ingave():
+#    df = pd.DataFrame(
+#        {
+#            "Datum": [],
+#            "Gas": [],
+#            "Water": [],
+#            "Electriciteit": []
+#        }
+#    )
+#    n = 0
+#    datum = date.today()
+#    while n != 5:
+#        datastring = input("Geef meterstand in datum,gas,water,elec:")
+#        df = df.append({"Datum": [datum], "Gas": [gas], "Water": [water], "Electriciteit": [ele]},ignore_index=True)
+#        print(n)
+#   df.to_csv(FILE_NAME,mode='a', index=False)
